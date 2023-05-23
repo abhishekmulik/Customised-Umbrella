ac0dc1467053f8acff31969ef7034278ab38ab42
Abhishek Mulik <mulikabhishek2605@gmail.com>
2023-05-23 22:52:34 +0530

    completed assignment

--- src/references.js
+++ src/references.js
@@ -1,5 +1,15 @@
-const colors = document.querySelector('.colors')
+const ColorRowEl = document.querySelector('.color-row')
+const imageUploadInputEl = document.getElementById('imageUploadInput');
+const uploadButtonEl = document.getElementById('upload-btn');
+const previewImageEl = document.getElementById('previewImage')
+const umbrellaImgEl = document.getElementById('umbrella-image')
+const loaderIconEl = document.getElementById('loader-icon')
 
-export default{
-    colors 
+export default {
+    ColorRowEl,
+    imageUploadInputEl,
+    uploadButtonEl,
+    previewImageEl,
+    umbrellaImgEl,
+    loaderIconEl
 }
--- src/script.js
+++ src/script.js
@@ -1,25 +1,78 @@
-import references from "./references.js"
+import references from "./references.js";
 
-const {colors} = references
-console.log(colors)
+const {ColorRowEl,
+    imageUploadInputEl,
+    uploadButtonEl,
+    previewImageEl,
+    umbrellaImgEl,
+    loaderIconEl} = references
+
+//array of colors
 const  arrOfColors= ['#2d2926','#707372','#003057','#d0006f','#00a3e0','#fed141','#e87722','#ef3340']
 
-const createDiv = () =>{
-    const newDiv = document.createElement("div")
-    return function(color){
-        newDiv.style.backgroundColor=color
-        // console.log(newDiv)
-       colors.appendChild(newDiv.cloneNode(true))
-       console.log(colors)
+const createDivUtil = () => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add('color-div');   
+     
+    return function(color) {
+        newDiv.style.backgroundColor = color;
+        newDiv.setAttribute('id', color);
+        ColorRowEl.appendChild(newDiv.cloneNode(true));
+    };
+  };
+
+
+//Add event listeners
+ColorRowEl.addEventListener('click', function(event) {
+    if (event.target.matches('.color-div')) {
+      const color = event.target.id;
+      changeColor(color);
     }
+  });
+
+
+//mimic loading functionality
+function loader(color){
+    umbrellaImgEl.style.display='none'
+    loaderIconEl.style.display='block'
+    loaderIconEl.style.fill=color
+    setTimeout(()=>{
+        loaderIconEl.style.display='none'
+        umbrellaImgEl.style.display='block'
+    },3000)
 }
 
-const createNewDiv = createDiv()
 
+//change colors
+function changeColor(color) {
+    loader(color)
+    umbrellaImgEl.src=`../assets/${color.slice(1)}.png`
+    uploadButtonEl.style.setProperty('--btn-color',color)
+}
+
+
+const createNewDiv = createDivUtil()
+//create the multiple colors options
 arrOfColors.forEach(color=>{
     createNewDiv(color)
 })
 
-console.log(colors)
 
-
+//upload image
+uploadButtonEl.addEventListener('click', function() {
+    const file = imageUploadInputEl.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function() {
+        const image = new Image();
+        image.style.width='100%'
+        image.src = reader.result;
+        previewImageEl.innerHTML = '';
+        loader(loaderIconEl.style.fill)
+        previewImageEl.appendChild(image);
+      };
+      reader.readAsDataURL(file);
+    } else {
+        previewImageEl.innerHTML = 'No image selected';
+    }
+  });
